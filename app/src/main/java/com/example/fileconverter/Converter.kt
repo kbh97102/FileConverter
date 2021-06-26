ff6976becb6c3fd00ace2d9c54b619295f445b8a
Arakene <arakene_dev@naver.com>
2021-06-26 18:25:06 +0900

    Add converter

---
+++ app/src/main/java/com/example/fileconverter/Converter.kt
@@ -0,0 +1,54 @@
+package com.example.fileconverter
+
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.FileInputStream
+import java.nio.ByteBuffer
+import java.nio.charset.Charset
+import java.nio.charset.CharsetDecoder
+
+class Converter {
+
+    fun detectCharset(): Charset? {
+
+        val charsetList = arrayListOf<Charset>()
+        charsetList.add(Charsets.US_ASCII)
+        charsetList.add(Charsets.UTF_32)
+        charsetList.add(Charsets.UTF_16LE)
+
+        for (type in charsetList) {
+            val file = File("./src/test.txt")
+            val input = BufferedInputStream(FileInputStream(file))
+            println("Current Type $type")
+            val decoder = type.newDecoder()
+            decoder.reset()
+
+            var buffer = ByteArray(file.length().toInt())
+            var decoded = false
+            while ((input.read(buffer) != -1) && !decoded) {
+                decoded = identify(buffer, decoder)
+            }
+
+            input.close()
+
+            if (decoded) {
+                return type
+            } else {
+                println("Decode fail $type")
+            }
+        }
+        return null
+    }
+
+    private fun identify(buffer: ByteArray, decoder: CharsetDecoder): Boolean {
+        try {
+            val charBuffer = decoder.decode(ByteBuffer.wrap(buffer))
+            println(charBuffer.toString())
+        } catch (e: CharacterCodingException) {
+            return false
+        }
+        return true
+    }
+
+
+}
--- app/src/main/java/com/example/fileconverter/MainActivity.kt
+++ app/src/main/java/com/example/fileconverter/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.example.fileconverter
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
+import java.nio.charset.Charset
+import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
