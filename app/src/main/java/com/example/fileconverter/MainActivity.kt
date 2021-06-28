aba0b053f2341260811e09b9170187d97f591000
Arakene <arakene_dev@naver.com>
2021-06-28 23:53:28 +0900

    Add file chooser

--- app/src/main/java/com/example/fileconverter/MainActivity.kt
+++ app/src/main/java/com/example/fileconverter/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.example.fileconverter
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.ByteArrayOutputStream
 import java.io.File
 import java.io.FileOutputStream
@@ -9,10 +13,28 @@ import java.nio.charset.Charset
 import java.nio.charset.StandardCharsets
 
 class MainActivity : AppCompatActivity() {
+
+    private val requestActivity = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ){activityResult ->
+
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        testButton.setOnClickListener {
+            openFile()
+        }
     }
+
+    fun openFile(){
+        val intent = Intent(Intent.ACTION_OPEN_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+        }
+        requestActivity.launch(intent)
+    }
+
 }
